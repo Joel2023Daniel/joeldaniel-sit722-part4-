785b9038f429e8f142a0590658e685f20bed0f09
Joel2023Daniel <s219504809@deakin.edu.au>
2024-09-04 18:08:06 +1000

    First commit with .gitignore and relevant terraform files removed

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#
+# Deploys a microservice to Kubernetes.
+#
+# Assumes the image has already been built and published to the container registry.
+#
+# Environment variables:
+#
+#   CONTAINER_REGISTRY - The hostname of your container registry.
+#   NAME - The name of the microservice to deploy.
+#   VERSION - The version of the microservice being deployed.
+#   DATABASE_URL - The PostgreSQL Database that will be used to store data.
+#
+# Usage:
+#
+#   ./scripts/deploy.sh
+#
+
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+: "$NAME"
+: "$VERSION"
+: "$DATABASE_URL"
+
+envsubst < ./scripts/kubernetes/${NAME}_deployment.yaml | kubectl apply -f -
